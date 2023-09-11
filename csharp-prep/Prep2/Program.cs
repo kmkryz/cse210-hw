63928612df2920167d3499f0b4045410f24a9f08
kmkryz <kmkryz@gmail.com>
2023-09-11 14:54:47 -0400

    prep 1 and prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        float number = int.Parse(userInput);
+
+        string letterGrade= "";
+
+        if (number >= 90)
+        {
+            letterGrade="A";
+
+        }
+        else if (number >= 80)
+        {
+            letterGrade="B";
+        }
+        else if (number >= 70)
+        {
+            letterGrade="C";
+        }
+        else if (number >= 60)
+        {
+            letterGrade="D";
+        }
+        else 
+        {
+            letterGrade="F";
+        }
+    
+        Console.WriteLine($"Your letter grade for this course: {letterGrade}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry you did not pass, better luck next time!");
+        }
+}
 }
