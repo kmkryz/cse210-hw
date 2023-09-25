b2b5db165dded1a94fcac3c1f04a4bcaa9bedc80
kmkryz <kmkryz@gmail.com>
2023-09-25 14:00:48 -0400

    learning02 task

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Philadelphia Country Club";
+        job1._jobTitle = "Golf Staff";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "New Country Motors";
+        job2._jobTitle = "Business Development Representative";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+
+        Resume resume = new Resume();
+        resume._name = "Kaitlyn-marie Kryzanski";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
+
+
     }
 }
