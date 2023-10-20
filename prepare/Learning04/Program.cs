37289eb22b75b96dc0f5dc9b15de3138aaab62f9
kmkryz <kmkryz@gmail.com>
2023-10-20 11:20:47 -0400

    learning 4 activities

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+
+        Assignment assignment1 = new Assignment("Joe Jonas", "Science");
+        Console.WriteLine(assignment1.GetSummary());
+
+
+        MathAssignment assignment2 = new MathAssignment("Enry Jones", "Math","342", "26-43");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+
+        WritingAssignment assignment3 = new WritingAssignment("Marie Antoinette", "Writing", "The Cheery gulfs");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,20 @@
+
+
+public class WritingAssignment : Assignment{
+
+    private string _title = "";
+
+
+    public WritingAssignment(string name, string topic, string title) : base(name, topic){
+
+        _title = title;
+
+    }
+
+    public string GetWritingInformation(){
+
+        string studentName = _studentName;
+
+        return $"{_title} by {studentName}";
+    }
+}
