3a99884634bb565c9fd046fbf973f8c5a4acab81
kmkryz <kmkryz@gmail.com>
2023-12-09 22:11:17 -0500

    details

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -7,10 +7,10 @@ class Program
         
 
         Address address1 = new Address("1768 Main St", "Fairhaven", "CA", "12345");
-        Lecture lectureEvent = new Lecture("Tech Talk", "Exciting tech discussion featuring the\nnewest updates for TBoT Max!", "01-15-2023", "3:00 PM", address1, "Thomas Rankin", 50);
+        Lecture lectureEvent = new Lecture("Runway Tech Talk", "Exciting tech discussion featuring the\nnewest updates for TBoT Max!", "01-15-2023", "3:00 PM", address1, "Thomas Rankin", 50);
 
         Address address2 = new Address("456 Park Ave", "Cocoaville", "FL", "67890");
-        Reception receptionEvent = new Reception("Networking Mixer", "Casual networking event for\nnewest employees with Obexum", "02-23-2023", "5:30 PM", address2, "rsvp@example.com");
+        Reception receptionEvent = new Reception("Luspy Networking Mixer", "Casual networking event for\nour newest employees", "02-23-2023", "5:30 PM", address2, "rsvp@example.com");
 
         Address address3 = new Address("729 Outkin Rd", "Carpe Village", "TX", "13579");
         OutdoorGathering outdoorEvent = new OutdoorGathering("Community 4th of July Picnic", "Family-friendly picnic,\nfireworks, BBQ, and fun!", "07-02-2023", "12:00 PM", address3, "Sunny with a high of 75°F");
